e4828c38eb9d9a8f5eed522e72a85c5afa0d6a5a
Aameer <officialaameer001@gmail.com>
2025-03-24 00:02:45 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+});
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT} `);
+});
